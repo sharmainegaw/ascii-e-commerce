4e0aa985c59eb3c862b3c612b6539d27256916e8
Sharmaine <sharmainegaw@gmail.com>
2022-05-20 15:08:31 +0800

    added formatting of date and price

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import { useState, useEffect } from "react";
 import { Grid } from "@mui/material";
 import ProductCard from "./components/ProductCard";
 
+const dateConvert = 1000 * 3600 * 24;
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
 function App() {
   const [currentData, setCurrentData] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -29,17 +34,32 @@ function App() {
       setLoading(false);
     });
   }, []);
+  
+
+  function formatPrice(price) {
+    return price.toFixed(2)
+  }
+  
+  function formatDate(dateString) {
+    const postDate = new Date(new Date(dateString).toDateString());
+    const currentDate = new Date(new Date(Date.now()).toDateString());
+    const deltaDays = ((currentDate - postDate.getTime()) / dateConvert).toFixed();
+
+    return deltaDays == 1 ? deltaDays + " day ago" : deltaDays <= 7 ?
+      deltaDays + " days ago" :
+      monthNames[postDate.getMonth()] + " " + postDate.getDate() + ", " + postDate.getFullYear();
+  }
 
   return (
     <div className='App'>
       <header>
-          <h1>Products Grid</h1>
+        <h1>Products Grid</h1>
 
-          <p>But first, a word from our sponsors:</p>
-          <img
-            className='ad'
-            src={`http://localhost:8000/ads/?r=${Math.floor(Math.random()*1000)}`}
-            alt='ad'/>
+        <p>But first, a word from our sponsors:</p>
+        <img
+          className='ad'
+          src={`http://localhost:8000/ads/?r=${Math.floor(Math.random()*1000)}`}
+          alt='ad'/>
       </header>
 
       {loading && <div>Loading...</div>}
@@ -49,7 +69,11 @@ function App() {
         {currentData &&
           currentData.map(({ id, face, price, size, date }) => (
             <Grid item xs={2} key={id}>
-              <ProductCard face={face} price={price} size={size} date={date}/>
+              <ProductCard
+                face={face}
+                price={formatPrice(price)}
+                size={size}
+                date={formatDate(date)}/>
             </Grid>
           ))}
       </Grid>
