94c99aed8a5900af9639a7b50000d0778b2a20a9
Sharmaine <sharmainegaw@gmail.com>
2022-05-20 15:07:44 +0800

    added design

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,16 +1,29 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import { Card, CardContent, Grid, Typography } from "@mui/material";
+import Box from '@mui/material/Box';
 
 export default function ProductCard(props) {
+
   return (
-    <Card>
-      <CardContent>
-        <Typography align="center" sx={{ fontSize: props.size }}>
-          {props.face}
-        </Typography>
-        <p>{props.price}</p>
-        <p>{props.size}</p>
-        <p>{props.date}</p>
-      </CardContent>
-    </Card>
+    <Box sx={{boxShadow: 2}}>
+      <Card>
+        <CardContent>
+          <Typography align="center" sx={{ fontSize: props.size }}>
+            {props.face}
+          </Typography>
+          <Grid container justifyContent="space-between">
+            <div>
+              <Typography variant="overline" display="block">
+                size
+              </Typography>
+              {props.size}
+            </div>
+            <p>${formatPrice(props.price)}</p>
+          </Grid>
+          <Typography align="right" variant="caption" display="block">
+            {formatDate(props.date)}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
