c509e1c950dd4fe8516a61aa09254641e2314eac
Sharmaine <sharmainegaw@gmail.com>
2022-05-21 11:27:08 +0800

    changed layout, added cardmedia

--- src/components/AdCard.js
+++ src/components/AdCard.js
@@ -1,18 +1,19 @@
-import { Card, CardContent, Grid, Typography } from "@mui/material";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 import Box from '@mui/material/Box';
 
 export default function AdCard(props) {
   return (
     <Box sx={{boxShadow: 2}}>
       <Card>
+        <CardMedia
+          component="img"
+          alt="green iguana"
+          image={`http://localhost:8000/ads/?r=${props.imageId}`}
+        />
         <CardContent>
           <Typography variant="overline">
-            sponsored
+            sponsored ad
           </Typography>
-          <img
-            className='ad'
-            src={`http://localhost:8000/ads/?r=${props.imageId}`}
-            alt='ad'/>
         </CardContent>
       </Card>
     </Box>
