899d1bc5b3f33430681f8ddde0c59556fd297b7c
Sharmaine <sharmainegaw@gmail.com>
2022-05-21 13:39:10 +0800

    added roboto font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import ProductList from './components/ProductList';
 import { Container } from '@mui/system';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <div className='App'>
       <header>
-        <h1>Products Grid</h1>        
+        <Container>
+          <Typography variant="h5" color="secondary">
+            <h1>Products Grid</h1>
+          </Typography>
+        </Container>
       </header>
       <Container>
         <ProductList/>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import 'typeface-roboto';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif'
+    ].join(','),
+  }
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
