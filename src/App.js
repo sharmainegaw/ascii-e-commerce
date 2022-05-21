1fc9a9fa8c038c671c0acbe092c6960e8effd71e
Sharmaine <sharmainegaw@gmail.com>
2022-05-21 10:22:22 +0800

    removed ad

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ function App() {
   return (
     <div className='App'>
       <header>
-        <h1>Products Grid</h1>
-
-        <p>But first, a word from our sponsors:</p>
-        <img
-          className='ad'
-          src={`http://localhost:8000/ads/?r=${Math.floor(Math.random()*1000)}`}
-          alt='ad'/>
+        <h1>Products Grid</h1>        
       </header>
       
       <ProductList/>
